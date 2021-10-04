2ea593cfb4ad7fccbe484697835e4ba990e670f4
Shekar <gv.shekar@gmail.com>
2021-10-03 23:29:37 -0400

    single process

--- scripts/leap_run.py
+++ scripts/leap_run.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     cluster = LSFCluster(name='sumo_ga', 
                interface='ib0', queue='short', #n_workers=WORKERS,
                cores=CORES, memory=f'{MEMORY}GB', job_extra=['-R select[rh=8]'],
-               walltime='4:00', ncpus=CORES,
+               walltime='4:00', ncpus=CORES, processes=1,
                )
     scale = math.ceil((WORKERS*1.0)/CORES)
     cluster.scale(cores=WORKERS)
